31887273d5d38e0e133e6582e0d1fea83d76d1a2
Dinesh Shaw <idineshshaw7@gmail.com>
2022-09-14 13:10:40 +0530

    navbar container

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -25,38 +25,40 @@ const Navbar = () => {
     </a>
   );
   return (
-    <div className="navbar">
-      <Link href="/">
-        <Image
-          src={logo}
-          className="navbar-logo"
-          alt="client Background"
-          draggable={false}
-          priority={true}
-          width={80}
-          height={62.5}
-        />
-      </Link>
-      <div className="menu">
-        <div
-          className={`menu-icon ${openMenu ? "opened" : "closed"}`}
-          onClick={handleOpenMenu}
-        >
-          <div className="line1"></div>
-          <div className="line2"></div>
-          <div className="line3"></div>
-        </div>
-        <div className={`menu-links ${openMenu ? "open" : "close"}`}>
-          <MenuLink href="#snacks">Snacks</MenuLink>
-          <MenuLink href="#breakfast">Breakfast</MenuLink>
-          <MenuLink href="#chinese">Chinese</MenuLink>
-          <MenuLink href="#tandoori">Tandoori</MenuLink>
-          <MenuLink href="#bread">Bread</MenuLink>
-          <MenuLink href="#rice">Rice</MenuLink>
-          <MenuLink href="#veg">Veg Course</MenuLink>
-          <MenuLink href="#non-veg">Non-Veg Course</MenuLink>
-          <MenuLink href="#desserts">Desserts</MenuLink>
-          <MenuLink href="#beverages">Beverages</MenuLink>
+    <div className="navbar-container">
+      <div className="navbar">
+        <Link href="/">
+          <Image
+            src={logo}
+            className="navbar-logo"
+            alt="client Background"
+            draggable={false}
+            priority={true}
+            width={80}
+            height={62.5}
+          />
+        </Link>
+        <div className="menu">
+          <div
+            className={`menu-icon ${openMenu ? "opened" : "closed"}`}
+            onClick={handleOpenMenu}
+          >
+            <div className="line1"></div>
+            <div className="line2"></div>
+            <div className="line3"></div>
+          </div>
+          <div className={`menu-links ${openMenu ? "open" : "close"}`}>
+            <MenuLink href="#snacks">Snacks</MenuLink>
+            <MenuLink href="#breakfast">Breakfast</MenuLink>
+            <MenuLink href="#chinese">Chinese</MenuLink>
+            <MenuLink href="#tandoori">Tandoori</MenuLink>
+            <MenuLink href="#bread">Bread</MenuLink>
+            <MenuLink href="#rice">Rice</MenuLink>
+            <MenuLink href="#veg">Veg Course</MenuLink>
+            <MenuLink href="#non-veg">Non-Veg Course</MenuLink>
+            <MenuLink href="#desserts">Desserts</MenuLink>
+            <MenuLink href="#beverages">Beverages</MenuLink>
+          </div>
         </div>
       </div>
     </div>
